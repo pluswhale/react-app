7755db1f6016eb739f9168f63935ad47e5d91b87
Egor Dultsev <listenmy@yandex.ru>
2022-04-04 16:04:21 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useMemo, useRef, useState} from 'react';
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+import ClassPost from "./components/ClassPost";
+import PostItem from "./components/PostItem";
+import ClassPostList from "./components/ClassPostList";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/buttons/MyButton";
+import MyInput from "./components/UI/input/MyInput";
+import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/Select/MySelect";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/Mymodal/MyModal";
+import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
+    const [posts, setPosts] = useState([])
+    /*states*/
+    const [filter, setFilter] = useState({sort: '', query: ''});
+    const [modal, setModal] = useState(false);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+    const createPost = (newPost) => {
+        setPosts([...posts, newPost])
+        setModal(false)
+    }
+
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <button onClick={fetchPosts}>GET DATA</button>
+        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+            Create User
+        </MyButton>
+        <MyModal visible={modal} setVisible={setModal}>
+            <PostForm create={createPost}/>
+        </MyModal>
+        <hr style={{margin: '15px 0'}}/>
+        <PostFilter
+            filter={filter}
+            setFilter={setFilter}
+        />
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Post for JS"/>
     </div>
   );
 }
